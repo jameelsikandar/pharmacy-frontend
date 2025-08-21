9346e29a0f3586fbf61ff18265f348713e725e35
Jameel Sikandar <jameelsikandarr@gmail.com>
2025-08-22 03:28:04 +0500

    add aos and login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-function App() {
-  
+import { useEffect } from "react";
+import LoginPage from "./pages/Login/LoginPage"
+import AOS from 'aos'
+import "aos/dist/aos.css";
 
+
+
+function App() {
+  useEffect(() => {
+    AOS.init({
+      disable: 'phone',           // Disable animations on phones
+      duration: 1000,              // Animation duration in ms
+      easing: 'ease-out-cubic',  // Animation easing
+      once: true                 // Whether animation should happen only once
+    });
+  }, []);
   return (
     <>
-     <h1 className="text-center text-red-500">Hello World</h1>
+     <LoginPage />
     </>
   )
 }
